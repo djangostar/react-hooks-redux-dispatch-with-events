ecb4ab13326301f20da9014866516af26cb79691
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2023-03-06 22:49:37 -0800

    more practice

--- src/reducer.js
+++ src/reducer.js
@@ -1,28 +1,26 @@
 // add code snippets from README
-// let state = { count: 0 };
-
 let state;
 
 function reducer(state = { count: 0 }, action) {
-  switch(action.type) {
+  switch (action.type) {
     case "counter/increment":
-      return { count: state.count + 1 }
+      return { count: state.count + 1 };
     default:
       return state;
   }
 }
 
 function dispatch(action) {
-  state = reducer(state, action)
-  render()
+  state = reducer(state, action);
+  render();
 }
 
 function render() {
   const container = document.getElementById("container");
-  container.textContent = state.count
+  container.textContent = state.count;
 }
 
-dispatch({ type: "@@INIT" });
+dispatch({type: '@@INIT'})
 
 const button = document.getElementById("button");
 
